038fa8ffecc2525e7c2f49bd4f9364c7d682f56e
3keneko <lucaslej19@gmail.com>
2023-11-07 22:05:58 +0100

    Coding lanes.cpp and lanes.hpp slowly but surely!

--- lanes.cpp
+++ lanes.cpp
@@ -1,1 +1,14 @@
 #include "lanes.hpp"
+
+int MovingObject::getSpeed() {
+    return speed;
+}
+
+bool Car::collide(Frog& frog) {
+    if (speed > 0)
+        return frog.getColumn() <= head &&
+            frog.getColumn() >= head - size;
+    else
+        return frog.getColumn() >= head &&
+            frog.getColumn() <= head + size;
+}
