d796271ff7962749fd274243a430ab3fe9483dcc
3keneko <lucaslej19@gmail.com>
2023-11-24 13:00:30 +0100

    Pushing last changes

--- src/Model/movingobjects.hpp
+++ src/Model/movingobjects.hpp
@@ -7,19 +7,24 @@ class MovingObject {
         const unsigned int size;
         const int speed;
         unsigned int head;
+        const unsigned int lane_id;
     public:
-        MovingObject(const int speed, unsigned int head, const unsigned int size):
-            speed(speed), head(head), size(size) {};
+        MovingObject(const int speed, unsigned int head, const unsigned int size, const unsigned lane_id):
+        speed(speed), head(head), size(size), lane_id(lane_id) {};
+
         virtual bool collide(Frog& frog);
         void move();
-        const int getSpeed() const { return speed; };
+        unsigned getSize() const { return size; }
+        unsigned getId() const { return lane_id; }
+        unsigned getHead() const { return head; }
+        const int getSpeed() const { return speed; }
         virtual ~MovingObject() {};
 };
 
 class Car: public MovingObject {
     public:
-        Car(int speed, unsigned int head, const unsigned int size) :
-            MovingObject(speed, head, size) {};
+        Car(int speed, unsigned int head, const unsigned int size, const unsigned lane_id) :
+            MovingObject(speed, head, size, lane_id) {};
         ~Car() {};
 };
 
@@ -27,8 +32,8 @@ class Turtle: public MovingObject {
     private:
         bool diving;
     public:
-        Turtle(int speed, const unsigned int head, const unsigned int size):
-            MovingObject(speed, head, size), diving(false) {};
+        Turtle(int speed, const unsigned int head, const unsigned int size, const unsigned lane_id):
+            MovingObject(speed, head, size, lane_id), diving(false) {};
         void dive() { diving=true; }
         void undive() { diving=false; }
         bool collide(Frog& frog) final override;
@@ -37,8 +42,8 @@ class Turtle: public MovingObject {
 
 class Log: public MovingObject {
     public:
-        Log(int speed, unsigned int head, const unsigned int size):
-            MovingObject(speed, head, size) {};
+        Log(int speed, unsigned int head, const unsigned int size, const unsigned lane_id):
+            MovingObject(speed, head, size, lane_id) {};
         ~Log() {};
 };
 
