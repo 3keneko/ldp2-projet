b9b1956494b3aed19ea9ab770eb1b20cd8035875
3keneko <lucaslej19@gmail.com>
2023-12-02 10:26:24 +0100

    Showing logs

---
+++ src/constants.hpp
@@ -0,0 +1,19 @@
+#ifndef CONSTANTS_H_
+#define CONSTANTS_H_
+
+namespace constants {
+    namespace window {
+        const int WIDTH = 500;
+        const int HEIGHT = 500;
+    }
+    namespace lanes {
+        const int NUMBER = 24;
+        const int HEIGHT = 20;
+    }
+    namespace frog {
+        const int WIDTH = 20;
+        const int HEIGHT = 20;
+    }
+}
+
+#endif // CONSTANTS_H_
