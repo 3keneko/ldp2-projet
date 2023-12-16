93500a19980a9af83516b0a0d3f6ba7d6b490703
3keneko <lucaslej19@gmail.com>
2023-12-16 12:15:47 +0100

    Commenting the debug part

---
+++ src/Model/boardmodel.cpp
@@ -0,0 +1,28 @@
+#include "boardmodel.hpp"
+#include "lanes.hpp"
+#include <memory>
+#include <algorithm>
+
+
+/// THIS MACRO IS DEFINED FOR DEBUGGING, IT HELPS
+/// PRINT OUT THE TUPLE V MORE EASILY
+/// IT IS TO BE DELETED BEFORE WE SEND THE PROJECT BACK TO MR. IACONO
+#define PRINTTUP(V) std::get<0>(V) << " " << std::get<1>(V)
+
+bool BoardModel::any_collision(Frog& frog) {
+    for (auto& lane: lanes) {
+        if (lane->getId() == frog.getLane()) {
+            auto try_rl = std::dynamic_pointer_cast<RoadLane>(lane);
+            if (try_rl != nullptr) {
+                auto car_list = try_rl->getCars();
+                return std::any_of(car_list.begin(), car_list.end(),
+                                   [&frog](std::shared_ptr<Car> car)
+                                   { /*std::cout << "Frog is " << frog.getColumn()
+                                               << " " << frog.getLane()
+                                               << " Car is " << PRINTTUP(car->getBoundaries()) << std::endl;*/
+                                       return car->collide(frog); });
+            }
+        }
+    }
+    return false;
+}
