9210d6c4f5f705057f752f2e17f1df7f430644d6
3keneko <lucaslej19@gmail.com>
2023-12-20 13:39:19 +0100

    tried adding a read from file function, not currently working

--- src/main.cpp
+++ src/main.cpp
@@ -16,6 +16,8 @@ int main(int argc, char *argv[]) {
   MainInit maininit {};
   std::srand(static_cast<unsigned>(time(nullptr)));
   MainInit m {};
+  // m.init_from_file("levels/level1.csv");
+  m.classic_init();
   auto ml = std::make_unique<MainLoop>(m.getBoardModel(), m.getBoardView(), m.getFrogView(), m.getFrog());
   MainWindow window(std::move(ml));
   window.show(argc, argv);
--- src/maininit.hpp
+++ src/maininit.hpp
@@ -10,7 +10,7 @@
 #include <memory>
 #include <vector>
 #include "Model/frog.hpp"
-
+#include <string>
 
 class MainInit {
     private:
@@ -19,45 +19,11 @@ class MainInit {
         std::shared_ptr<BoardModel> bm;
         std::shared_ptr<FrogView> fv;
     public:
-        MainInit() {
-            // Intializing the frog
-            Frog frog {0, 250};
-            frg = std::make_shared<Frog>(frog);
-            fv = std::make_shared<FrogView>(frg);
+        MainInit() {}
 
-            // Initializing the lanes
-            std::vector<std::shared_ptr<Lane>> lanes;
-            lanes.push_back(std::make_shared<SafeLane>(1));
-            lanes.push_back(std::make_shared<SafeLane>(2));
-            lanes.push_back(std::make_shared<RoadLane>(3, 3, 150, 350, 0, 30, 1));
-            lanes.push_back(std::make_shared<RoadLane>(4, 3, 150, 350, 0, 30, 2));
-            lanes.push_back(std::make_shared<RoadLane>(5, 10, 150, 350, 0, 40, 3));
-            lanes.push_back(std::make_shared<RoadLane>(6, 3, 150, 350, 0, 40, -2));
-            lanes.push_back(std::make_shared<SafeLane>(7));
-            lanes.push_back(std::make_shared<TurtleLane>(8, 5, 10, 200, 0, 50, 1));
-            lanes.push_back(std::make_shared<TurtleLane>(9, 5, 10, 200, 0, 50, -1));
-            lanes.push_back(std::make_shared<LogLane>(10, 3, 30, 350, 2, 200, 1));
-            lanes.push_back(std::make_shared<LogLane>(11, 3, 30, 350, -2, 200, 2));
-            lanes.push_back(std::make_shared<LogLane>(12, 3, 30, 350, 2, 200, -1));
-            lanes.push_back(std::make_shared<FinishLane>(13));
+        void classic_init();
+        void init_from_file(std::string path_to_file);
 
-            std::vector<std::shared_ptr<MovingObjectLane>> v1 { };
-            for (auto& lane: lanes) {
-                auto moving_object_lane = std::dynamic_pointer_cast<MovingObjectLane>(lane);
-                if (moving_object_lane != nullptr) {
-                    v1.push_back(moving_object_lane);
-                }
-            }
-
-            std::vector<std::shared_ptr<LaneView>> v {};
-
-            for (auto& lane: lanes) {
-                v.push_back(LaneView::makeView(lane));
-            }
-            // Putting the frog and the lanes together; initializing the board
-            bm = std::make_shared<BoardModel>(lanes);
-            board = std::make_shared<BoardView>(v, bm) ;
-        }
         std::shared_ptr<BoardView> getBoardView() {
             return board;
         }
