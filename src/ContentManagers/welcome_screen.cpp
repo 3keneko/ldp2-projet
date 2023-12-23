6a47f7a0b3940f34a763ba86ed8850e441838eaf
3keneko <lucaslej19@gmail.com>
2023-12-23 13:16:15 +0100

    no files left behind

---
+++ src/ContentManagers/welcome_screen.cpp
@@ -0,0 +1,19 @@
+#include "welcome_screen.hpp"
+#include "gameloop.hpp"
+
+void start_game_callback(Button* button, void*) {
+    button->make_pay();
+}
+
+void WelcomeScreen::draw() {
+    start_game_button->draw();
+    Text welcome { "Lenny & Lucas game", 100, 200 };
+    welcome.draw();
+}
+
+void WelcomeScreen::informManager() {
+    if (start_game_button->getPayoff() != actions::NOTHING) {
+        auto g = std::make_unique<GameLoop>( "../levels/level1.csv" );
+        cm->start_game(std::move(g));
+    }
+}
