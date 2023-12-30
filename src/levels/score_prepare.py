0b00021a54549089cf92fb1ffea09758fd93dc09
3keneko <lucaslej19@gmail.com>
2023-12-30 14:23:05 +0100

    Cleaning up the score files

--- src/levels/score_prepare.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-import os
-
-for files in os.listdir():
-    if files.endswith(".csv"):
-        # os.remove(files)
-        with open(f"{files[:-4]}_score.csv", 'w', encoding="utf-8") as f:
-            f.write("0")
