762e64264bf0ed0123d96ed22993af2dd6c677b1
3keneko <lucaslej19@gmail.com>
2024-01-09 15:18:04 +0100

    Updating report

--- src/ContentManagers/level_select.hpp
+++ src/ContentManagers/level_select.hpp
@@ -49,7 +49,7 @@ class LevelSelect: public WindowContents {
             this
     };
 
-        Text best_score_show { "Highest Score: ", // + std::to_string(ss.getHighScore().getScore()),
+        Text best_score_show { "Highest Score: ",
            constants::window::WIDTH/4,
            constants::window::HEIGHT * 4/5,
            30,
@@ -74,47 +74,10 @@ class LevelSelect: public WindowContents {
             ss.getFromFile();
             best_score_show.setString("Highest Score: " + std::to_string(ss.getHighScore().getScore()));
         }
-        // void informManager() override;
-        void draw() override {
-            increase_lvl.draw();
-            reduce_lvl.draw();
-            play_game.draw();
-            lvl_show.draw();
-            reset_score.draw();
-            best_score_show.draw();
-        }
-
-        void manageButtonPush(int x, int y) override {
-            if (selected_level < max_level) increase_lvl.manageClick(x, y);
-            reduce_lvl.manageClick(x, y);
-            play_game.manageClick(x, y);
-            reset_score.manageClick(x, y);
-        }
-
-        unsigned getLevel() {
-            return selected_level;
-        }
-
-        void manageAction(actions& action) override {
-            if (action == actions::DIMIN && selected_level > 1) {
-                selected_level--;
-            } else if (action == actions::INCR) {
-                selected_level++;
-            } else if (action == actions::RESET) {
-                ss.getFromFile();
-                ss.resetHighScore();
-                ss.writeToFile();
-            }
-            ss.setLevel(selected_level);
-            best_score_show.setString("Highest Score: " + std::to_string(ss.getHighScore().getScore()));
-            lvl_show.setString("Selected level: " + std::to_string(selected_level));
-            if (action == actions::STARTAFTERSEL) {
-                if (auto cm_lock = cm.lock()) {
-                    auto new_game = std::make_unique<GameLoop>(selected_level);
-                    cm_lock->startGame(std::move(new_game));
-                }
-            }
-        }
+        void draw() override;
+        void manageButtonPush(int x, int y) override;
+        unsigned getLevel();
+        void manageAction(actions& action) override;
         ~LevelSelect() {}
 
 };
